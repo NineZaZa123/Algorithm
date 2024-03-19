d562167cd4615b9f1bad93adf4aa5a5ae7439279
NinelnwZa007 <163229612+TanatSamuttha@users.noreply.github.com>
2024-03-19 14:41:12 +0700

    Update Countpath.cpp

--- Countpath.cpp
+++ Countpath.cpp
@@ -8,19 +8,16 @@ ll dp[N][N];
 
 int main(){
     ios_base::sync_with_stdio(0),cin.tie(0);
-    cin >> q;
-    while(q--){
-        cin >> n >> m;
-        dp[1][1]=1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(i==1&&j==1)continue;
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                dp[i][j]%=MOD;
-            }
+    cin >> n >> m;
+    dp[1][1]=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(i==1&&j==1)continue;
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            dp[i][j]%=MOD;
         }
-        cout << dp[n][m] << '\n';
     }
+    cout << dp[n][m] << '\n';
 
     return 0;
 }
