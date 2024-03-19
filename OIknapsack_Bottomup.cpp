6d5aaadd65c85128479a4b154676c8adb0e39236
NinelnwZa007 <163229612+TanatSamuttha@users.noreply.github.com>
2024-03-19 14:43:21 +0700

    Update OIknapsack_Bottomup.cpp

--- OIknapsack_Bottomup.cpp
+++ OIknapsack_Bottomup.cpp
@@ -2,29 +2,26 @@
 using namespace std;
 
 const int N=1001;
-int q,n,w;
+int n,w;
 int val[N],we[N];
 int dp[N];
 int ans;
 
 int main(){
     ios_base::sync_with_stdio(0),cin.tie(0);
-    cin >> q;
-    while(q--){
-        cin >> n >> w;
-        for(int i=1; i<=n; i++)cin >> val[i];
-        for(int i=1; i<=n; i++)cin >> we[i];
-        ans=0;
-        for(int i=0; i<=w; i++)dp[i]=0;
-        for(int i=1; i<=n; i++){
-            for(int j=w; j>=0; j--){
-                if(j<we[i])continue;
-                dp[j]=max(dp[j],dp[j-we[i]]+val[i]);
-                ans=max(ans,dp[j]);
-            }
+    cin >> n >> w;
+    for(int i=1; i<=n; i++)cin >> val[i];
+    for(int i=1; i<=n; i++)cin >> we[i];
+    ans=0;
+    for(int i=0; i<=w; i++)dp[i]=0;
+    for(int i=1; i<=n; i++){
+        for(int j=w; j>=0; j--){
+            if(j<we[i])continue;
+            dp[j]=max(dp[j],dp[j-we[i]]+val[i]);
+            ans=max(ans,dp[j]);
         }
-        cout << ans << '\n';
     }
+    cout << ans << '\n';
     
     return 0;
 }
