d25529d08e2af2c8d690b921fc5d6c97f066c6ca
NinelnwZa007 <163229612+TanatSamuttha@users.noreply.github.com>
2024-03-19 14:41:52 +0700

    Update Longest_Common_Subsequence.cpp

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -8,20 +8,17 @@ string a,b;
 
 int main(){
     ios_base::sync_with_stdio(0),cin.tie(0);
-    cin >> q;
-    while(q--){
-        cin >> n >> m >> a >> b;
-        a="x"+a;
-        b="x"+b;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                dp[i][j]=0;
-                if(a[i]==b[j])dp[i][j]=dp[i-1][j-1]+1;
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+    cin >> n >> m >> a >> b;
+    a="x"+a;
+    b="x"+b;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            dp[i][j]=0;
+            if(a[i]==b[j])dp[i][j]=dp[i-1][j-1]+1;
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
-        cout << dp[n][m] << '\n';
     }
+    cout << dp[n][m] << '\n';
 
     return 0;
 }
